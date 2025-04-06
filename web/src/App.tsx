303a4ee82a25740b10089ce3e12ae218e374af7d
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2025-04-05 21:26:39 -0700

    Change env

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -30,12 +30,13 @@ const queryClient = new QueryClient();
 const networks = {
 	devnet: { url: getFullnodeUrl('devnet') },
 	mainnet: { url: getFullnodeUrl('mainnet') },
+  testnet: { url: getFullnodeUrl('testnet') },
 };
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-    <SuiClientProvider networks={networks} defaultNetwork="devnet">
+    <SuiClientProvider networks={networks} defaultNetwork="testnet">
       <WalletProvider>
         <Router>
           <Routes>
